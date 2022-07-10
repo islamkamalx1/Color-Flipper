781a4251d94b3f09e4425056c71800ce2ad9e501
Islam Kamal <islamkamalx1@gmail.com>
2022-07-10 16:35:24 +0200

    update

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+document.body.innerHTML = `<nav>
+<div class="nav-center">
+  <h4>color flipper</h4>
+  <ul class="nav-links">
+    <li>
+      <a href="index.html">simple</a>
+    </li>
+    <li>
+      <a href="hex.html">hex</a>
+    </li>
+  </ul>
+</div>
+</nav>
+<main>
+<div class="container">
+  <h2>
+    background color :
+    <span class="color">
+      #f1f5f8
+    </span>
+  </h2>
+  <button class="btn btn-hero" id="btn">click me</button>
+</div>
+</main>`;
+
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
